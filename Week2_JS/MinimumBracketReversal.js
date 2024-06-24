0770e148881f19af181dee1eb5d9a081ea0d630a
Harshul <88796921+harshul26@users.noreply.github.com>
2024-06-24 18:49:45 +0530

    Update MinimumBracketReversal.js

--- Week2_JS/MinimumBracketReversal.js
+++ Week2_JS/MinimumBracketReversal.js
@@ -1,1 +1,19 @@
+const minimumBracketReversal = (expression) => {
+    let arr = [];
+    let exp = expression.split("");
+    exp.forEach(element => {
+        if (element === '{') {
+            arr.push(element);
+        } else if (element === '}') {
+            arr.pop();
+        }
+    });
 
+    if (arr.length % 2 == 0) {
+        return arr.length / 2;
+    } else {
+        return -1;
+    }
+}
+let expression = '{{{{}}'
+console.log(minimumBracketReversal(expression));
