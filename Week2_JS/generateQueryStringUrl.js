1ea018cb0aa60e8d35a5921e9bba03fb877f21ca
Harshul <88796921+harshul26@users.noreply.github.com>
2024-06-24 18:47:22 +0530

    Update generateQueryStringUrl.js

--- Week2_JS/generateQueryStringUrl.js
+++ Week2_JS/generateQueryStringUrl.js
@@ -1,1 +1,21 @@
+const object = {
+    "keyOne": "value One",
+    "keyTwo": "value Two",
+    "keyThree": "value Three",
+}
 
+const url = "https://localhost:400";
+
+
+const queryGenerator = (object, url) => {
+    let queryUrl = `${url}` + "?";
+    let arr = Object.keys(object);
+    arr.forEach((key) => {
+        let val = object[key];
+        queryUrl += `${key} = ${val}&`;
+    })
+    queryUrl = queryUrl.substring(0, queryUrl.length - 1);
+    return queryUrl;
+}
+
+console.log(queryGenerator(object, url));
