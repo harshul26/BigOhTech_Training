57675b212d7814f0fb5197c1f25ee736e238b66a
Harshul <88796921+harshul26@users.noreply.github.com>
2024-06-24 18:48:26 +0530

    Update JsonManipulations1.js

--- Week2_JS/JsonManipulations1.js
+++ Week2_JS/JsonManipulations1.js
@@ -1,1 +1,56 @@
+const jsonManipulate = (array) => {
 
+    const result = {};
+    array.forEach((obj) => {
+        const { batch_id, ...rest } = obj;
+        if (result.hasOwnProperty(batch_id)) {
+            result[batch_id].push(rest);
+
+        } else {
+            result[batch_id] = [rest];
+
+        }
+        console.log(result);
+
+    });
+
+    return result;
+}
+
+let arr = [{
+        "batch_id": "123",
+        "name": "Tony",
+        "contact": "9872276210"
+    },
+    {
+        "batch_id": "231",
+        "name": "Steve",
+        "contact": "7876543210"
+    },
+    {
+        "batch_id": "123",
+        "name": "Bruce",
+        "contact": "6776543210"
+    },
+    {
+        "batch_id": "321",
+        "name": "Clint",
+        "contact": "8954643210"
+    },
+    {
+        "batch_id": "123",
+        "name": "Peter",
+        "contact": "7666543210"
+    },
+    {
+        "batch_id": "231",
+        "name": "Phil",
+        "contact": "8896543210"
+    },
+    {
+        "batch_id": "321",
+        "name": "Nick",
+        "contact": "9876521210"
+    }
+];
+console.log(jsonManipulate(arr));
