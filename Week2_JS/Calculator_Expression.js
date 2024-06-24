6a1b85944e0b3c3e056d02ca805189723bc34e34
Harshul <88796921+harshul26@users.noreply.github.com>
2024-06-24 18:44:12 +0530

    Update Calculator_Expression.js

--- Week2_JS/Calculator_Expression.js
+++ Week2_JS/Calculator_Expression.js
@@ -1,1 +1,22 @@
+const calculator = (expression) => {
 
+    Array.from(expression).forEach((element) => {
+        if (typeValidator(Number(element), 'number') && operatorValidator(element) === false) {
+            console.log("error" + element + typeof(element));
+            new Error('invalid element in expression');
+        }
+    });
+    return eval(expression);
+
+}
+
+const typeValidator = (number, type) => {
+    return typeof(it) === type;
+}
+
+const operatorValidator = (op) => {
+    return ['+', '-', '*', '/', '%', '(', ')', '[', ']'].includes(op);
+}
+
+
+console.log(calculator("1+(2+3)*4-10/2"));
