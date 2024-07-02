139286819d104b4872d61b546ea6bbeae9b55f39
Harshul <88796921+harshul26@users.noreply.github.com>
2024-07-02 12:49:50 +0530

    Update script.js

--- ECommerWebsite/script.js
+++ ECommerWebsite/script.js
@@ -1,4 +1,4 @@
-//scripts.js
+// scripts.js
 
 document.addEventListener('DOMContentLoaded', () => {
     const apiUrl = 'https://dummyjson.com';
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let limit = 20;
     let totalProducts = 0;
     let currentCategory = '';
-    let searchQuery = '';
+    let isFetching = false; // To prevent multiple simultaneous fetch requests
 
     const searchBar = document.getElementById('search-bar');
     const searchBtn = document.getElementById('search-btn');
@@ -15,21 +15,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const limitSelect = document.getElementById('limit-select');
     const prevBtn = document.getElementById('prev-btn');
     const nextBtn = document.getElementById('next-btn');
-    const loadMoreBtn = document.getElementById('load-more-btn');
+    const prevBtnTop = document.getElementById('prev-btn-top');
+    const nextBtnTop = document.getElementById('next-btn-top');
+    const pageNumber = document.getElementById('page-number');
+    const pageNumberTop = document.getElementById('page-number-top');
+    const paginationNumbers = document.getElementById('pagination-numbers');
+    const paginationNumbersTop = document.getElementById('pagination-numbers-top');
 
     searchBtn.addEventListener('click', searchProduct);
     limitSelect.addEventListener('change', updateLimit);
     prevBtn.addEventListener('click', () => changePage(-1));
     nextBtn.addEventListener('click', () => changePage(1));
-    loadMoreBtn.addEventListener('click', loadMoreProducts);
+    prevBtnTop.addEventListener('click', () => changePage(-1));
+    nextBtnTop.addEventListener('click', () => changePage(1));
+    window.addEventListener('scroll', handleScroll);
 
     function fetchCategories() {
         fetch(`${apiUrl}/products/categories`)
-            .then((response) => response.json())
-            .then((categories) => {
-                categories.forEach((category) => {
+            .then(response => response.json())
+            .then(categories => {
+                categories.forEach(category => {
                     const categoryDiv = document.createElement('div');
-                    categoryDiv.textContent = category['name']; // Display the category name
+                    categoryDiv.textContent = category['name'];
                     categoryDiv.addEventListener('click', () => fetchProductsByCategory(category['name']));
                     categoriesSection.appendChild(categoryDiv);
                 });
@@ -37,34 +44,43 @@ document.addEventListener('DOMContentLoaded', () => {
             .catch(error => console.error('Error fetching categories:', error));
     }
 
-    function fetchProducts(limit, skip, category = '', search = '', append = false) {
+    function fetchProducts(limit, skip, category = '', append = false) {
+        isFetching = true;
         let url = `${apiUrl}/products?limit=${limit}&skip=${skip}`;
         if (category) {
-            url = `${apiUrl}/products/category/${category}?limit=${limit}&skip=${skip}`;
-        }
-        if (search) {
-            url = `${apiUrl}/products/search?q=${search}&limit=${limit}&skip=${skip}`;
+            url += `&category=${category}`;
         }
         fetch(url)
             .then(response => response.json())
             .then(data => {
                 totalProducts = data.total;
                 displayProducts(data.products, append);
+                updatePagination();
+                isFetching = false;
             })
-            .catch(error => console.error('Error fetching products:', error));
+            .catch(error => {
+                console.error('Error fetching products:', error);
+                isFetching = false;
+            });
     }
 
     function searchProduct() {
-        searchQuery = searchBar.value;
-        currentPage = 1;
-        fetchProducts(limit, 0, currentCategory, searchQuery);
+        const query = searchBar.value;
+        if (query) {
+            fetch(`${apiUrl}/products/search?q=${query}`)
+                .then(response => response.json())
+                .then(data => {
+                    displayProducts(data.products);
+                    updatePagination();
+                })
+                .catch(error => console.error('Error searching products:', error));
+        }
     }
 
     function fetchProductsByCategory(category) {
-        currentPage = 1;
         currentCategory = category;
-        searchQuery = '';
-        fetchProducts(limit, 0, category, searchQuery);
+        currentPage = 1;
+        fetchProducts(limit, 0, category);
     }
 
     function displayProducts(products, append = false) {
@@ -105,21 +121,61 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function updateLimit() {
         limit = parseInt(limitSelect.value, 10);
-        currentPage = 1;
-        fetchProducts(limit, 0, currentCategory, searchQuery);
+        fetchProducts(limit, 0, currentCategory);
     }
 
     function changePage(direction) {
+        if (currentPage < 1) {
+            prevBtn.disabled = true;
+        } else {
+            prevBtn.disabled = false;
+        }
         if ((direction === -1 && currentPage > 1) || (direction === 1 && (currentPage * limit) < totalProducts)) {
             currentPage += direction;
-            fetchProducts(limit, (currentPage - 1) * limit, currentCategory, searchQuery);
+            fetchProducts(limit, (currentPage - 1) * limit, currentCategory);
         }
     }
 
-    function loadMoreProducts() {
-        const skip = currentPage * limit;
-        currentPage += 1;
-        fetchProducts(limit, skip, currentCategory, searchQuery, true);
+    function handleScroll() {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 500 && !isFetching) {
+            currentPage++;
+            fetchProducts(limit, (currentPage - 1) * limit, currentCategory, true);
+        }
+    }
+
+    function updatePagination() {
+        //pageNumber.textContent = ` ${currentPage}`;
+        pageNumberTop.textContent = `${currentPage}`;
+
+        // Clear previous pagination numbers
+        paginationNumbers.innerHTML = '';
+        paginationNumbersTop.innerHTML = '';
+
+        const totalPages = Math.ceil(totalProducts / limit);
+        const maxVisiblePages = 5; // Number of visible page numbers
+
+        let startPage = Math.max(1, currentPage - Math.floor(maxVisiblePages / 2));
+        let endPage = Math.min(totalPages, startPage + maxVisiblePages - 1);
+
+        if (endPage - startPage + 1 < maxVisiblePages) {
+            startPage = Math.max(1, endPage - maxVisiblePages + 1);
+        }
+
+        for (let i = startPage; i <= endPage; i++) {
+            const pageLink = document.createElement('button');
+            pageLink.textContent = i;
+            pageLink.style.cursor = 'pointer';
+            pageLink.addEventListener('click', () => goToPage(i));
+            paginationNumbers.appendChild(pageLink.cloneNode(true));
+            paginationNumbersTop.appendChild(pageLink);
+        }
+    }
+
+    function goToPage(page) {
+        if (page !== currentPage && page >= 1 && page <= Math.ceil(totalProducts / limit)) {
+            currentPage = page;
+            fetchProducts(limit, (currentPage - 1) * limit, currentCategory);
+        }
     }
 
     // Initial fetch
